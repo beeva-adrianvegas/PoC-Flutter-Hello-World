eabb1ae2288fe19ceae3b56d84343c22c595b95b
adrian.vegas <adrian.vegas@bbva.com>
2018-11-14 18:36:27 +0100

    Creating a widget

--- lib/main.dart
+++ lib/main.dart
@@ -1,22 +1,32 @@
 import 'package:flutter/material.dart';
 import 'strings.dart';
 
-void main() => runApp(new MyApp());
+void main() => runApp(new PocFlutterApp());
 
-class MyApp extends StatelessWidget {
+class PocFlutterApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return new MaterialApp(
-      title: 'GHFlutter',
-      home: new Scaffold(
-        appBar: new AppBar(
-          title: new Text(Strings.appTitle),
-        ),
-        body: new Center(
-          child: new Text(Strings.appMidText),
-        ),
+      title: Strings.appTitle,
+      home: new PoCFlutter(),
+    );
+  }
+}
+
+class PoCFlutter extends StatefulWidget {
+  @override
+  createState() => new PoCFlutterState();
+}
+
+class PoCFlutterState extends State<PoCFlutter> {
+  @override
+  Widget build(BuildContext context) {
+    return new Scaffold (
+      appBar: new AppBar(
+        title: new Text(Strings.appTitle),
       ),
+      body: new Text(Strings.appMidText),
     );
   }
 }
