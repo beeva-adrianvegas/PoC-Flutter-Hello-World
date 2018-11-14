0de5ebb397c0ce7c74c322ab75c8f8cf3c419958
adrian.vegas <adrian.vegas@bbva.com>
2018-11-14 18:53:02 +0100

    Adding network calls

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,8 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+
 import 'strings.dart';
 
 void main() => runApp(new PocFlutterApp());
@@ -20,6 +24,26 @@ class PoCFlutter extends StatefulWidget {
 }
 
 class PoCFlutterState extends State<PoCFlutter> {
+
+  //underscore makes the members of the class private
+  var _members = [];
+  final _biggerFont = const TextStyle(fontSize: 18.0);
+
+  _loadData() async {
+    //Los miembros de nuestra organizacion son privados asi que mostraremos los de flutter
+    String dataURL = "https://api.github.com/orgs/flutter/members";
+    http.Response response = await http.get(dataURL);
+    setState(() {
+      _members = json.decode(response.body);
+    });
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    _loadData();
+  }
+
   @override
   Widget build(BuildContext context) {
     return new Scaffold (
